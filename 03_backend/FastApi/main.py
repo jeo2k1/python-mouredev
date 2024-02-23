cae15b4c9b3ff131e98a17db980bb019aaa1f24d
JeO <jeo2k1@gmail.com>
2024-02-23 12:30:52 -0300

    hasta recursos estaticos

--- 03_backend/FastApi/main.py
+++ 03_backend/FastApi/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI # Importamos FastApi
 from routers import products, users # Importamos los modulos de rutas que hemos creado.
+from fastapi.staticfiles import StaticFiles # Agregamos la clase para manejar archivos estáticos.
 
 app = FastAPI() # Creamos la instancia de FastApi
 
 # Agregamos los rutas a nuestra aplicación
 app.include_router(products.router) # Agrego la ruta de products
 app.include_router(users.router) # Agrego la ruta de users
-
+app.mount("/static", StaticFiles(directory='static'), name = "static") # Monta el directorio /static en /static del servidor
 
 @app.get("/") # Decorador para definir una ruta GET en /
 async def root(): #  Es una función asincrona, por lo que se utiliza "async"
